b0de5d88f3f3da4c6bb03230c7f8cda67212db3c
Elvira Latypova <ellalatypova666@gmail.com>
2025-01-05 21:40:02 +0500

    Update animation

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -5,10 +5,10 @@ import "./work.scss";
 
 const cardVariants = {
   offscreen: {
-    y: 660,
+    opacity: 0,
   },
   onscreen: {
-    y: 0,
+    opacity: 1,
     transition: {
       type: "spring",
       bounce: 0.4,
